eaa732e0eae9f735fd4db5039079cc967f7fce02
ntk221 <nittakdub@gmail.com>
2023-07-04 22:51:00 +0900

    using tracing for logging

--- my-todo/src/main.rs
+++ my-todo/src/main.rs
@@ -1,10 +1,16 @@
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
+use std::env;
 
 #[tokio::main]
 async fn main() {
+    let log_level = env::var("RUST_LOG").unwrap_or_else(|_| "info".to_owned());
+    env::set_var("RUST_LOG", &log_level);
+    tracing_subscriber::fmt::init();
+
     let app = Router::new().route("/", get(root));
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000)); // std::convert::Fromトレイトを実装している. Fromは，引数にとった値を自身の型に変換する
+    tracing::debug!("Listening on {}", addr);
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
@@ -15,3 +21,19 @@ async fn main() {
 async fn root() -> &'static str {
     "Hello, World!"
 }
+
+
+/*
+#[stable(feature = "addr_from_into_ip", since = "1.17.0")]
+impl<I: Into<IpAddr>> From<(I, u16)> for SocketAddr {
+    /// Converts a tuple struct (Into<[`IpAddr`]>, `u16`) into a [`SocketAddr`].
+    ///
+    /// This conversion creates a [`SocketAddr::V4`] for an [`IpAddr::V4`]
+    /// and creates a [`SocketAddr::V6`] for an [`IpAddr::V6`].
+    ///
+    /// `u16` is treated as port of the newly created [`SocketAddr`].
+    fn from(pieces: (I, u16)) -> SocketAddr {
+        SocketAddr::new(pieces.0.into(), pieces.1)
+    }
+}
+ */
