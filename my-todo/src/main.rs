dcd0d18d4b0a5e152d1aee911cf0012bebc78559
ntk221 <nittakdub@gmail.com>
2023-07-05 00:03:37 +0900

    routingとhandlerのテスト

--- my-todo/src/main.rs
+++ my-todo/src/main.rs
@@ -1,4 +1,10 @@
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Router, Json,
+    http::StatusCode,
+    response::IntoResponse
+};
+use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 use std::env;
 
@@ -8,7 +14,9 @@ async fn main() {
     env::set_var("RUST_LOG", &log_level);
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(root));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000)); // std::convert::Fromトレイトを実装している. Fromは，引数にとった値を自身の型に変換する
     tracing::debug!("Listening on {}", addr);
 
@@ -18,22 +26,73 @@ async fn main() {
         .unwrap();
 }
 
+fn create_app() -> Router {
+    Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user))
+}
+
 async fn root() -> &'static str {
     "Hello, World!"
 }
 
+async fn create_user(Json(payload): Json<CreateUser> ) -> impl IntoResponse {
+    let user = User {
+        id: 1337,
+        username: payload.username
+    };
 
-/*
-#[stable(feature = "addr_from_into_ip", since = "1.17.0")]
-impl<I: Into<IpAddr>> From<(I, u16)> for SocketAddr {
-    /// Converts a tuple struct (Into<[`IpAddr`]>, `u16`) into a [`SocketAddr`].
-    ///
-    /// This conversion creates a [`SocketAddr::V4`] for an [`IpAddr::V4`]
-    /// and creates a [`SocketAddr::V6`] for an [`IpAddr::V6`].
-    ///
-    /// `u16` is treated as port of the newly created [`SocketAddr`].
-    fn from(pieces: (I, u16)) -> SocketAddr {
-        SocketAddr::new(pieces.0.into(), pieces.1)
+    (StatusCode::CREATED, Json(user))
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+struct CreateUser {
+    username: String
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+struct User {
+    id: u64,
+    username: String
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{header, Method, Request},
+    };
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn should_return_hello_world() {
+        let req = Request::builder().uri("/").body(Body::empty()).unwrap();
+        let res = create_app().oneshot(req).await.unwrap();
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        assert_eq!(bytes, "Hello, World!");
+    }
+
+    #[tokio::test]
+    async fn should_return_user_data() {
+        let req = Request::builder()
+            .uri("/users")
+            .method(Method::POST)
+            .header(header::CONTENT_TYPE, "application/json")
+            .body(Body::from(r#"{"username":"foo"}"#))
+            .unwrap();
+        let res = create_app().oneshot(req).await.unwrap();
+
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let body: String = String::from_utf8(bytes.to_vec()).unwrap();
+        let user: User = serde_json::from_str(&body)
+            .expect("failed to deserialize body");
+        assert_eq!(
+            user, 
+            User {
+                id: 1337,
+                username: "foo".to_owned()
+            }
+        );
     }
 }
- */
